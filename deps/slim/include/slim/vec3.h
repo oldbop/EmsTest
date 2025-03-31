87c5077b6399fa4397664e7146518a481f5ec2c6
oldbop <oliverdavybowker@gmail.com>
2025-03-31 19:40:05 +0100

    Use inline functions instead

--- deps/slim/include/slim/vec3.h
+++ deps/slim/include/slim/vec3.h
@@ -3,18 +3,96 @@
 
 #include "types.h"
 
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void  vec3_print(const vec3 *v);
-vec3  vec3_nor(const vec3 *v);
-float vec3_mag(const vec3 *v);
-float vec3_dot(const vec3 *v1, const vec3 *v2);
-vec3  vec3_crs(const vec3 *v1, const vec3 *v2);
-vec3  vec3_add(const vec3 *v1, const vec3 *v2);
-vec3  vec3_sub(const vec3 *v1, const vec3 *v2);
-vec3  vec3_sml(const vec3 *v, float scalar);
+inline void vec3_print(const vec3 *v) {
+  printf("%.1f %.1f %.1f\n", v->v[0], v->v[1], v->v[2]);
+}
+
+inline vec3 vec3_nor(const vec3 *v) {
+
+  float mag = sqrtf((v->v[0] * v->v[0]) +
+                    (v->v[1] * v->v[1]) +
+                    (v->v[2] * v->v[2]));
+
+  float inv_mag = (mag == 0.0f) ? 0.0f : 1.0f / mag;
+
+  vec3 res = {
+    v->v[0] * inv_mag, 
+    v->v[1] * inv_mag, 
+    v->v[2] * inv_mag
+  };
+
+  return res;
+}
+
+inline float vec3_mag(const vec3 *v) {
+
+  float res = sqrtf((v->v[0] * v->v[0]) +
+                    (v->v[1] * v->v[1]) +
+                    (v->v[2] * v->v[2]));
+
+  return res;
+}
+
+inline float vec3_dot(const vec3 *v1, const vec3 *v2) {
+
+  float res = (v1->v[0] * v2->v[0]) +
+              (v1->v[1] * v2->v[1]) +
+              (v1->v[2] * v2->v[2]);
+
+  return res;
+}
+
+inline vec3 vec3_crs(const vec3 *v1, const vec3 *v2) {
+
+  vec3 res = {
+    (v1->v[1] * v2->v[2]) - (v1->v[2] * v2->v[1]),
+    (v1->v[2] * v2->v[0]) - (v1->v[0] * v2->v[2]),
+    (v1->v[0] * v2->v[1]) - (v1->v[1] * v2->v[0])
+  };
+
+  return res;
+}
+
+inline vec3 vec3_add(const vec3 *v1, const vec3 *v2) {
+
+  vec3 res = {
+    v1->v[0] + v2->v[0],
+    v1->v[1] + v2->v[1],
+    v1->v[2] + v2->v[2]
+  };
+
+  return res;
+}
+
+inline vec3 vec3_sub(const vec3 *v1, const vec3 *v2) {
+
+  vec3 res = {
+    v1->v[0] - v2->v[0],
+    v1->v[1] - v2->v[1],
+    v1->v[2] - v2->v[2]
+  };
+
+  return res;
+}
+
+inline vec3 vec3_sml(const vec3 *v, float scalar) {
+
+  vec3 res = {
+    v->v[0] * scalar,
+    v->v[1] * scalar,
+    v->v[2] * scalar
+  };
+
+  return res;
+}
 
 #ifdef __cplusplus
 }
--- deps/slim/src/mat4.c
+++ deps/slim/src/mat4.c
@@ -1,145 +1,21 @@
 #include <slim/mat4.h>
 
-#include <math.h>
-#include <stdint.h>
-#include <stdio.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-void mat4_print(const mat4 *m) {
+void mat4_print(const mat4 *m);
+mat4 mat4_idy(void);
+mat4 mat4_tsp(const mat4 *m);
+mat4 mat4_add(const mat4 *m1, const mat4 *m2);
+mat4 mat4_sub(const mat4 *m1, const mat4 *m2);
+mat4 mat4_mul(const mat4 *m1, const mat4 *m2);
+mat4 mat4_scl(const vec3 *v);
+mat4 mat4_tst(const vec3 *v);
+mat4 mat4_rotX(float r);
+mat4 mat4_rotY(float r);
+mat4 mat4_rotZ(float r);
 
-  for (uint32_t i = 0; i < 4; ++i) {
-    printf("%.1f %.1f %.1f %.1f\n", m->v[(4 * i)],
-                                    m->v[(4 * i) + 1],
-                                    m->v[(4 * i) + 2],
-                                    m->v[(4 * i) + 3]);
-  }
-
-  printf("\n");
+#ifdef __cplusplus
 }
-
-mat4 mat4_idy(void) {
-
-  mat4 res = {
-        1.0f,     0.0f,     0.0f,     0.0f,
-        0.0f,     1.0f,     0.0f,     0.0f,
-        0.0f,     0.0f,     1.0f,     0.0f,
-        0.0f,     0.0f,     0.0f,     1.0f
-  };
-
-  return res;
-}
-
-mat4 mat4_tsp(const mat4 *m) {
-
-  mat4 res;
-
-  for (uint32_t i = 0; i < 4; ++i) {
-    for (uint32_t j = 0; j < 4; ++j) {
-      res.v[(4 * i) + j] = m->v[i + (4 * j)];
-    }
-  }
-
-  return res;
-}
-
-mat4 mat4_add(const mat4 *m1, const mat4 *m2) {
-
-  mat4 res;
-
-  for (uint32_t i = 0; i < 16; ++i) {
-    res.v[i] = m1->v[i] + m2->v[i];
-  }
-
-  return res;
-}
-
-mat4 mat4_sub(const mat4 *m1, const mat4 *m2) {
-
-  mat4 res;
-
-  for (uint32_t i = 0; i < 16; ++i) {
-    res.v[i] = m1->v[i] - m2->v[i];
-  }
-
-  return res;
-}
-
-mat4 mat4_mul(const mat4 *m1, const mat4 *m2) {
-
-  mat4 res = { 0 };
-
-  for (uint32_t i = 0; i < 4; ++i) {
-    for (uint32_t j = 0; j < 4; ++j) {
-      for (uint32_t k = 0; k < 4; ++k) {
-        res.v[(4 * i) + j] += m1->v[(4 * i) + k] * m2->v[j + (4 * k)];
-      }
-    }
-  }
-
-  return res;
-}
-
-mat4 mat4_scl(const vec3 *v) {
-
-  mat4 res = {
-     v->v[0],     0.0f,     0.0f,     0.0f,
-        0.0f,  v->v[1],     0.0f,     0.0f,
-        0.0f,     0.0f,  v->v[2],     0.0f,
-        0.0f,     0.0f,     0.0f,     1.0f
-  };
-
-  return res;
-}
-
-mat4 mat4_tst(const vec3 *v) {
-
-  mat4 res = {
-        1.0f,     0.0f,     0.0f,  v->v[0],
-        0.0f,     1.0f,     0.0f,  v->v[1],
-        0.0f,     0.0f,     1.0f,  v->v[2],
-        0.0f,     0.0f,     0.0f,     1.0f
-  };
-
-  return res;
-}
-
-mat4 mat4_rotX(float r) {
-
-  float sin_r = sinf(r), cos_r = cosf(r);
-
-  mat4 res = {
-        1.0f,     0.0f,     0.0f,     0.0f,
-        0.0f,    cos_r,   -sin_r,     0.0f,
-        0.0f,    sin_r,    cos_r,     0.0f,
-        0.0f,     0.0f,     0.0f,     1.0f
-  };
-
-  return res;
-}
-
-mat4 mat4_rotY(float r) {
-
-  float sin_r = sinf(r), cos_r = cosf(r);
-
-  mat4 res = {
-       cos_r,     0.0f,    sin_r,     0.0f,
-        0.0f,     1.0f,     0.0f,     0.0f,
-      -sin_r,     0.0f,    cos_r,     0.0f,
-        0.0f,     0.0f,     0.0f,     1.0f
-  };
-
-  return res;
-}
-
-mat4 mat4_rotZ(float r) {
-
-  float sin_r = sinf(r), cos_r = cosf(r);
-
-  mat4 res = {
-       cos_r,   -sin_r,     0.0f,     0.0f,
-       sin_r,    cos_r,     0.0f,     0.0f,
-        0.0f,     0.0f,     1.0f,     0.0f,
-        0.0f,     0.0f,     0.0f,     1.0f
-  };
-
-  return res;
-}
+#endif
