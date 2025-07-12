67ebe4ce6b794f2c25611fea5f270650cfd5d786
oldbop <oliverdavybowker@gmail.com>
2025-07-12 22:05:46 +0100

    Add aspect ratio member and setter to camera class

--- src/camera.hpp
+++ src/camera.hpp
@@ -8,6 +8,7 @@
 class Camera {
 public:
   mat4 get_transform(uint32 screen_width, uint32 screen_height) const;
+  void set_aspect(float aspect) { asp_ = aspect; }
 
 private:
   vec3 pos_ = { 0.0f, 0.0f, 2.0f };
@@ -17,6 +18,7 @@ private:
   float fovx_ = PI / 2.0f;
   float near_ = 0.1f;
   float far_  = 100.0f;
+  float asp_  = 1.0f;
 };
 
 #endif
