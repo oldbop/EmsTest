f621f55b0e6375d3d1478284c84200fde1468e8f
oldbop <oliverdavybowker@gmail.com>
2025-07-16 22:42:06 +0100

    Develop window class

--- src/window.cpp
+++ src/window.cpp
@@ -14,24 +14,43 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <optional>
 #include <string>
 
-bool Window::init(int32 width, int32 height, const std::string& title) {
+Window::~Window() {
+  if (ptr_)
+    glfwTerminate();
+}
+
+Window::Window(Window&& other) noexcept : Window() {
+  swap(other);
+  register_callbacks();
+}
+
+std::optional<Window> Window::create(int32 width, int32 height, const std::string& title) {
+
+  if (!glfwInit()) {
+    std::cout << "GLFW: failed to initialise" << std::endl;
+    return std::nullopt;
+  }
 
   //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow *win = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+  GLFWwindow *window_ptr = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
 
-  if (!win) {
+  if (!window_ptr) {
+    std::cout << "GLFW: failed to create window" << std::endl;
     glfwTerminate();
-    return false;
+    return std::nullopt;
   }
 
-  ptr_ = { win, glfwDestroyWindow };
+  Window window;
 
-  glfwMakeContextCurrent(ptr_.get());
+  window.ptr_ = window_ptr;
+
+  glfwMakeContextCurrent(window.ptr_);
   glfwSwapInterval(1);
 
 #ifndef SYS_GL_HEADERS
@@ -40,42 +59,52 @@ bool Window::init(int32 width, int32 height, const std::string& title) {
 
   int32 fb_width, fb_height;
 
-  glfwGetFramebufferSize(ptr_.get(), &fb_width, &fb_height);
+  glfwGetFramebufferSize(window.ptr_, &fb_width, &fb_height);
 
   glEnable(GL_DEPTH_TEST);
   glEnable(GL_CULL_FACE);
   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
   glViewport(0, 0, fb_width, fb_height);
 
-  cam_.set_aspect(static_cast<float>(fb_width) / static_cast<float>(fb_height));
+  window.cam_.set_aspect(static_cast<float>(fb_width) / static_cast<float>(fb_height));
 
-  glfwSetWindowUserPointer(ptr_.get(), this);
-
-  auto resize_cb = [](GLFWwindow *win, int32 width, int32 height) -> void {
-    static_cast<Window *>(glfwGetWindowUserPointer(win))->resize_cb(win, width, height);
-  };
-
-  auto key_cb = [](GLFWwindow *win, int32 key, int32 scan, int32 act, int32 mods) -> void {
-    static_cast<Window *>(glfwGetWindowUserPointer(win))->key_cb(win, key, scan, act, mods);
-  };
-
-  glfwSetFramebufferSizeCallback(ptr_.get(), resize_cb);
-  glfwSetKeyCallback(ptr_.get(), key_cb);
-
-  return true;
+  return Window(std::move(window));
 }
 
-void Window::resize_cb(GLFWwindow *win, int32 width, int32 height) {
+void Window::draw() {
+
+  glClear(GL_COLOR_BUFFER_BIT);
+  
+  glfwSwapBuffers(ptr_);
+  glfwPollEvents();
+}
+
+void Window::register_callbacks() {
+
+  glfwSetWindowUserPointer(ptr_, this);
+
+  auto resize_la = [](GLFWwindow *p, int32 width, int32 height) -> void {
+    static_cast<Window *>(glfwGetWindowUserPointer(p))->resize_callback(p, width, height);
+  };
+
+  auto key_la = [](GLFWwindow *p, int32 key, int32 scan, int32 act, int32 mods) -> void {
+    static_cast<Window *>(glfwGetWindowUserPointer(p))->key_callback(p, key, scan, act, mods);
+  };
+
+  glfwSetFramebufferSizeCallback(ptr_, resize_la);
+  glfwSetKeyCallback(ptr_, key_la);
+}
+
+void Window::resize_callback(GLFWwindow *p, int32 width, int32 height) {
+
   glViewport(0, 0, width, height);
   cam_.set_aspect(static_cast<float>(width) / static_cast<float>(height));
 }
 
-void Window::key_cb(GLFWwindow *win, int32 key, int32 scan, int32 act, int32 mods) {
+void Window::key_callback(GLFWwindow *p, int32 key, int32 scan, int32 act, int32 mods) {
 
-  if (key == GLFW_KEY_ESCAPE && act == GLFW_PRESS) {
-    glfwSetWindowShouldClose(win, 1);
-  }
-  if (key == GLFW_KEY_SPACE && act == GLFW_PRESS) {
+  if (key == GLFW_KEY_ESCAPE && act == GLFW_PRESS)
+    glfwSetWindowShouldClose(p, 1);
+  if (key == GLFW_KEY_SPACE && act == GLFW_PRESS)
     std::cout << "Space pressed!!" << std::endl;
-  }
 }
--- src/window.hpp
+++ src/window.hpp
@@ -6,19 +6,37 @@
 
 #include <GLFW/glfw3.h>
 
-#include <memory>
+#include <optional>
 #include <string>
+#include <utility>
 
 class Window {
 public:
-  Window() : ptr_(nullptr, nullptr) {}
-  bool init(int32 width, int32 height, const std::string& title);
+  ~Window();
+  Window(const Window&) = delete;
+  Window& operator=(const Window&) = delete;
+  Window(Window&& other) noexcept;
+  Window& operator=(Window&& other) = delete;
+
+  static std::optional<Window> create(int32 width, int32 height, const std::string& title);
+
+  bool should_close() { return glfwWindowShouldClose(ptr_); }
+  void draw();
 private:
-  void resize_cb(GLFWwindow *win, int32 width, int32 height);
-  void key_cb(GLFWwindow *win, int32 key, int32 scan, int32 act, int32 mods);
+  void register_callbacks();
+
+  void resize_callback(GLFWwindow *p, int32 width, int32 height);
+  void key_callback(GLFWwindow *p, int32 key, int32 scan, int32 act, int32 mods);
 
   Camera cam_;
-  std::unique_ptr<GLFWwindow, void (*)(GLFWwindow *)> ptr_;
+  GLFWwindow *ptr_ = nullptr;
+
+  Window() = default;
+
+  void swap(Window& other) noexcept {
+    std::swap(cam_, other.cam_);
+    std::swap(ptr_, other.ptr_);
+  }
 };
 
 #endif
