8aec93af90535cb30ae000cf198f32d13b141bf1
oldbop <oliverdavybowker@gmail.com>
2025-07-27 18:55:26 +0100

    Alter struct formatting

--- src/camera.hpp
+++ src/camera.hpp
@@ -9,9 +9,9 @@ struct Camera {
 
   mat4 get_transform(int32 width, int32 height) const;
 
-  vec3 pos;
-  vec3 tar;
-  vec3 ort;
+  vec3  pos;
+  vec3  tar;
+  vec3  ort;
 
   float fovx;
   float near;
